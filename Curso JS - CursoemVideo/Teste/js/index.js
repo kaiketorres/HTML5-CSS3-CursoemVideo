e2d1b16f0eca222268a36639128b43adc1c95e96
kaike torres <kaiketorrea@gmail.com>
2023-12-03 11:11:42 -0300

    adicionando meus estudos de js que estou fazendo pelo celular

---
+++ Curso JS - CursoemVideo/Teste/js/index.js
@@ -0,0 +1,13 @@
+window.confirm("Esta pronto?")
+
+var nome = window.prompt("Nome")
+
+var idade = window.prompt("idade")
+
+var n1 = Number(window.prompt("n1"))
+
+var n2 = Number(window.prompt("n2"))
+
+var s = n1 + n2
+
+window.alert(`${nome} ${idade} ${s}`)
